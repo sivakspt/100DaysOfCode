6ca571a01e55568bf278aae6be3ca7444e00c670
Anishcharith <anish.charith@gmail.com>
2017-12-08 08:43:51 +0530

    SMA correction

--- Day001/SMA.py
+++ Day001/SMA.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def SMA(price,period):
     sma=[]
-    sma+=list(np.zeros(period))
-    for i in range(period,len(price)):
-        sma.append(sum(price[i-period:i])/period)
+    sma+=list(np.zeros(period-1))
+    for i in range(period-1,len(price)):
+        sma.append(sum(price[i-period+1:i+1])/period)
     return sma
